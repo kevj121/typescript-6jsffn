4f161d7a9a75378b4abe594a833700fadfeda383
kevj121 <kevj121@gmail.com>
2022-10-27 00:18:52 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
-const lines = [{ line1: 'aaaa', line2: 'bbbb', line3: 'cccc' }];
+const lines = [{ line: 'bbbb'}, {line: 'zzzz'}, {line: '1111' }];
 
 lines.map((line) => {
   console.log(line);
 });
 
-
+const sorted = lines.sort((l1,l2) => l1.line.localeCompare(l2.line));
+sorted.map((line) => {
+  console.log(line);
+});
