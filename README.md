9ce47821e61b683f00c06b8da1e7ca63e6ea492a
kevj121 <kevj121@gmail.com>
2022-10-27 00:12:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-6jsffn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-3rxruf)
---
+++ index.ts
@@ -0,0 +1,5 @@
+const lines = [{ line1: 'aaaa', line2: 'bbbb', line3: 'cccc' }];
+
+lines.map((line) => {
+  console.log(line);
+});
